9c9b79d75c67336deb614accb02af419d364afca
MannAmbani1 <Mambani0123@conestogac.on.ca>
2025-03-17 14:29:40 -0400

    first commit

---
+++ .history/src/components/Contact_20250208001449.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SocialBtns from "./SocialBtns";
+import ContactInfo from "./ContactInfo";
+import ContactForm from "./ContactForm";
+
+export default function Contact({ data, socialData }) {
+  const { sectionHeading, contactImg, contactInfo } = data;
+  return (
+    <section
+      id="contactus"
+      className="section contactus-section mx-auto max-w-3xl"
+    >
+      <div className="container">
+        <div className="contactus-box rounded overflow-hidden bg-gray-100">
+          <div className="row g-0 p-4 p-lg-5">
+            <div className="col-lg-4" />
+            <div className="col-lg-8">
+              <div
+                className="contactus-title"
+                data-aos="fade-left"
+                data-aos-duration="1200"
+                data-aos-delay="200"
+              >
+                <h5>{sectionHeading.title}</h5>
+                <p className="m-0">{sectionHeading.subTitle}</p>
+              </div>
+            </div>
+          </div>
+          <div className="row g-0 contactus-form p-4 p-lg-5 flex-row-reverse">
+            <div className="col-lg-8">
+              <div className="contact-form">
+                <ContactForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+}
---
+++ .history/src/components/Contact_20250208020502.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import SocialBtns from "./SocialBtns";
+import ContactInfo from "./ContactInfo";
+import ContactForm from "./ContactForm";
+
+export default function Contact({ data, socialData }) {
+  const { sectionHeading, contactImg, contactInfo } = data;
+  return (
+    <section id="contactus" className="section contactus-section">
+      <div className="container contactus-container">
+        <div className="contactus-box rounded oveflow-hidden gray-bg col-lg-8">
+          <div className="row g-0 p-4 p-lg-5">
+            <div
+              className="contactus-title"
+              data-aos="fade-left"
+              data-aos-duration="1200"
+              data-aos-delay="200"
+            >
+              <h5>{sectionHeading.title}</h5>
+              <p className="m-0">{sectionHeading.subTitle}</p>
+            </div>
+            <ContactInfo contactInfoData={contactInfo} />
+          </div>
+          <div className="row g-0 contactus-form p-4 p-lg-5 flex-row-reverse">
+            <div className="contact-form">
+              <ContactForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
